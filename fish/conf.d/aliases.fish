1b57ac1de0539af624103361a045f3bc2817403c
skwi <cedric.spalvieri@gmail.com>
2018-04-05 15:36:16 +0200

    Better docker cleanup

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -9,7 +9,12 @@ alias duh='du -cksh *'
 alias sf='php bin/console'
 
 #docker
-alias clean-docker='docker stop (docker ps -q); docker rm (docker ps -aq); docker system prune --force'
+alias stop-docker='docker stop (docker ps -q)'
+alias clean-docker='docker system prune --volumes --force; docker container ls'
+alias reset-docker='stop-docker; clean-docker'
+
+#dockeriezed
+alias mysql-dockerized='docker run --name one-shot-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=painauchocolat -d mysql:5.5 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci'
 
 # Misc
 alias meteo='curl -4 "wttr.in/Lyon?2nQ"'
