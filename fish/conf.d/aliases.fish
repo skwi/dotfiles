eca066f0c685056251816e2f5f4b081293eeebca
skwi <cedric.spalvieri@gmail.com>
2018-08-02 09:05:58 +0200

    🔥 remove unused aliases

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -5,9 +5,6 @@ alias fuck='sudo !!'
 alias ducks='du -cks * | sort -rn | head'
 alias duh='du -cksh *'
 
-# Symfony
-alias sf='php bin/console'
-
 #docker
 alias stop-docker='docker stop (docker ps -q)'
 alias clean-docker='docker system prune --volumes --force; docker container ls'
@@ -15,6 +12,3 @@ alias reset-docker='stop-docker; clean-docker'
 
 #dockeriezed
 alias mysql-dockerized='docker run --name one-shot-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=painauchocolat -d mysql:5.5 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci'
-
-# Misc
-alias meteo='curl -4 "wttr.in/Lyon?2nQ"'
